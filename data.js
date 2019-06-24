67f273959a944fdf07005a1da9d13c1254948da6
Duro Akintunde <duroak@yahoo.com>
2019-06-24 12:15:41 -0500

    added data.js

---
+++ data.js
@@ -0,0 +1,103 @@
+const QUESTIONS = [
+    {
+        question: 'What premier league team won 13 premier league titles?',
+        answers: [ 
+            'Manchester City',
+            'Arsenal',
+            'Chelsea',
+            'Manchester United'
+        ],
+        correctAnswer: 'Manchester United',
+    },
+    {
+        question: 'Who is the most expensive transfer in the world?',
+        answers: [
+            'Neymar Jr.',
+            'Eden Hazard',
+            'Paul Pogba',
+            'Kylian Mbappe'
+        ], 
+        correctAnswer: 'Neymar Jr.',
+    },
+    {
+        question: 'What year did Spain win their first FIFA World Cup?',
+        answers: [
+            '1998',
+            '2010',
+            '2002',
+            '2018'
+        ],
+        correctAnswer: '2010',
+    },
+    {
+        question: 'Who Infamously Scored a goal with their hands and called it the \"The Hand of God\"?',
+        answers: [
+            'Lionel Messi',
+            'Diego Maradona',
+            'Ronaldo (Brazil)',
+            'Franz Beckenbauer'
+        ],
+        correctAnswer: 'Diego Maradona',
+    },
+    {
+        question: 'How many ballon d\'ors does Lionel Messi have?',
+        answers: [
+            '4',
+            '6',
+            '3',
+            '5'
+        ],
+        correctAnswer: '5',
+    },
+    {
+        question: 'What were the two teams that played in the Champions League Final in 2006?',
+        answers: [
+            'Milan v. Liverpool',
+            'Real Madrid v. Juventus',
+            'Barcelona v. Arsenal',
+            'Porto v. Monaco'
+        ],
+        correctAnswer: 'Barcelona v. Arsenal',
+    },
+    {
+        question: 'Which Nation does Eden Harzard play for?',
+        answers: [
+            'France',
+            'Netherlands',
+            'Germany',
+            'Belgium'
+        ],
+        correctAnswer: 'Belgium'
+    },
+    {
+        question: 'Who is current Manager of Newcastle United?',
+        answers: [
+            'Rafa Benitez',
+            'Jose Mourinho',
+            'Pep Guardiola',
+            'Eddie Howe'
+        ],
+        correctAnswer: 'Rafa Benitez',
+    },
+    {
+        question: 'What is the the nickname of Leicester City F.C.?',
+        answers: [
+            'Reds',
+            'Foxes',
+            'Wolves',
+            'The Swans'
+        ],
+        correctAnswer: 'Foxes',
+    },
+    {
+        question: 'Who is the top Scorer in Champions League history?',
+        answers: [
+            'Sammuel Eto\'o',
+            'Mo Salah',
+            'Cristiano Ronaldo',
+            'Antoine Griezmann'
+        ],
+        correctAnswer: 'Cristiano Ronaldo',
+    },
+]
+
